e590f010bf44d6754801de2f3275ed9c0f249594
iskysun96 <iskysun96@gmail.com>
2024-04-13 09:11:51 +0900

    challenge created

--- projects/python-challenge-3/smart_contracts/asa_vault/contract.py
+++ projects/python-challenge-3/smart_contracts/asa_vault/contract.py
@@ -1,7 +1,50 @@
-from algopy import ARC4Contract, arc4
-
+from algopy import ARC4Contract, arc4, UInt64, Asset, gtxn, itxn, Txn, Global,subroutine
 
 class AsaVault(ARC4Contract):
-    @arc4.abimethod()
-    def hello(self, name: arc4.String) -> arc4.String:
-        return "Hello, " + name
+    asset_id: UInt64
+    asa_balance: UInt64
+
+    # create the app and configure what ASA this vault will hold
+    @arc4.abimethod(allow_actions=["NoOp"], create="require")
+    def create_application(self, asset: Asset) -> None:
+        self.asset_id = asset.id
+        self.asa_balance = UInt64(0)
+
+    @subroutine
+    def authorize_creator(self) -> None:
+        assert Txn.sender == Global.creator_address
+
+    @arc4.abimethod
+    def opt_in_to_asset(self, mbr_pay: gtxn.PaymentTransaction) -> None:
+        self.authorize_creator()
+        assert not Global.current_application_address.is_opted_in(Asset(self.asset_id))
+
+        assert mbr_pay.receiver == Global.current_application_address
+        assert mbr_pay.amount == Global.min_balance + Global.asset_opt_in_min_balance
+        
+    @arc4.abimethod
+    def deposit_asa(self, deposit_txn: gtxn.AssetTransferTransaction)-> None: 
+        self.authorize_creator()
+        assert deposit_txn.asset_receiver == Global.current_application_address
+        assert deposit_txn.asset_amount > 0
+
+        self.asa_balance += deposit_txn.asset_amount
+
+    @arc4.abimethod
+    def withdraw_asa(self, asset: Asset) -> None:
+        self.authorize_creator()
+
+        itxn.AssetTransfer(
+            xfer_asset=self.asset_id,
+            asset_receiver=Global.creator_address,
+            asset_amount=0,
+            asset_close_to=Global.creator_address,
+            fee=0,
+        ).submit()
+
+        self.asa_balance = UInt64(0)
+
+    @arc4.abimethod(readonly=True)
+    def get_asa_balance(self) -> UInt64:
+        return self.asa_balance
+        
--- projects/python-challenge-3/smart_contracts/asa_vault/deploy_config.py
+++ projects/python-challenge-3/smart_contracts/asa_vault/deploy_config.py
@@ -3,6 +3,8 @@ import logging
 import algokit_utils
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
+from algosdk import transaction
+from algosdk.atomic_transaction_composer import TransactionWithSigner
 
 logger = logging.getLogger(__name__)
 
@@ -18,19 +20,83 @@ def deploy(
         AsaVaultClient,
     )
 
+    # create the ASA to deposit in the vault
+    sp = algod_client.suggested_params()
+
+    asa_create_txn = transaction.AssetConfigTxn(
+        sender=deployer.address,
+        sp=sp,
+        total=10,
+        decimals=0,
+        unit_name="SRO",
+        asset_name="Super RARE Oranges",
+        strict_empty_address_check=False,
+    )
+
+    stxn = asa_create_txn.sign(deployer.private_key)    
+    txid = algod_client.send_transaction(stxn)
+    results = transaction.wait_for_confirmation(algod_client, txid, 4)
+
+    created_asset = results["asset-index"]
+    print(f"The Super RARE Oranges is created and has asset id: {created_asset}")
+
+    # Instantiate the app client
     app_client = AsaVaultClient(
         algod_client,
         creator=deployer,
         indexer_client=indexer_client,
     )
 
-    app_client.deploy(
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
+    # Deploy the vault smart contract and assign the created ASA to it
+    response = app_client.create_create_application(asset=created_asset)
+    print(f"Created app with app id: {response.tx_info['application-index']}")
+
+    # Send the mbr amount to the vault and call the opt in method so that the vault can receive the ASA
+    mbr_pay = transaction.PaymentTxn(
+        sender=deployer.address,
+        sp=sp,
+        receiver=app_client.app_address,
+        amt= 200_000,
     )
-    name = "world"
-    response = app_client.hello(name=name)
-    logger.info(
-        f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.return_value}"
+
+    sp = algod_client.suggested_params()
+    sp.fee = 2000
+    sp.flat_fee = True
+
+    app_client.opt_in_to_asset(mbr_pay=TransactionWithSigner(mbr_pay, deployer.signer), transaction_parameters= algokit_utils.TransactionParameters(
+        foreign_assets=[created_asset],
+        suggested_params=sp
+    ))
+
+    # Deposit the ASA into the vault
+    deposit_txn  = transaction.AssetTransferTxn(
+        sender=deployer.address,
+        sp=sp,
+        receiver=app_client.app_address,
+        amt= 1,
+        index=created_asset,
     )
+
+    app_client.deposit_asa(deposit_txn=TransactionWithSigner(deposit_txn, deployer.signer))
+
+    # Check the balance of the vault
+    response = app_client.get_asa_balance()
+    print(f"The Vault holds {response.return_value} Super RARE Oranges")
+
+    app_client.deposit_asa(deposit_txn=TransactionWithSigner(deposit_txn, deployer.signer))
+
+    response = app_client.get_asa_balance()
+    print(f"The Vault holds {response.return_value} Super RARE Oranges")
+
+    sp = algod_client.suggested_params()
+    sp.fee = 2000
+    sp.flat_fee = True
+
+    # Withdraw the ASA from the vault
+    app_client.withdraw_asa(asset=created_asset, transaction_parameters= algokit_utils.TransactionParameters(
+        suggested_params=sp
+    ))
+
+    response = app_client.get_asa_balance()
+    print(f"The Vault holds {response.return_value} Super RARE Oranges")
+
